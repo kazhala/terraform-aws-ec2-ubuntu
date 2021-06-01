7c1057f61e07ea3197c81cf2d2f15dc05e7e45fb
Kevin Zhuang <kevin7441@gmail.com>
2021-06-01 23:23:29 +0000

    fix(lambda): index error

--- lambda/start.py
+++ lambda/start.py
@@ -7,14 +7,6 @@ waiter = client.get_waiter("instance_running")
 sns = boto3.client("sns")
 
 
-def get_instance_name(tags):
-    name = None
-    for tag in tags:
-        if tag["Key"] == "Name":
-            name = tag["Value"]
-    return name
-
-
 def lambda_handler(event, context):
     instance_id = os.getenv("INSTANCE")
     if not instance_id:
@@ -25,7 +17,5 @@ def lambda_handler(event, context):
     if os.getenv("TOPIC_ARN"):
         waiter.wait(InstanceIds=[instance_id])
         instance_details = client.describe_instances(InstanceIds=[instance_id])
-        message = instance_details["Reservations"][0]["Instances"][instance_id][
-            "PublicDnsName"
-        ]
+        message = instance_details["Reservations"][0]["Instances"][0]["PublicDnsName"]
         sns.publish(TopicArn=os.getenv("TOPIC_ARN"), Message=message, Subject="EC2 DNS")
